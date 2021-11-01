eecd3ab848b049ecbb24ba38af38b8c5f3790da3
Vito <vito95v@gmail.com>
2021-11-01 19:12:41 +0100

    Fine esercizio

---
+++ database/seeds/TripsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Trip;
+
+class TripsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 100; $i++) {
+            $new_trip = new Trip();
+            $new_trip->daparture = $faker->city();
+            $new_trip->destination = $faker->city();
+            $new_trip->departure_date = $faker->date();
+            $new_trip->return_date = $faker->date();
+            $new_trip->save();
+        }
+    }
+}
